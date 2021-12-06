adc2c0d393549ba1a1a2bd83a641eeab6b9d0294
EmirOzdemirPXL <emir.ozdemir@student.pxl.be>
2021-12-07 00:44:28 +0100

    Added the dotenv package and imported in the server.js file

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+// dotenv config
+require("dotenv").config();
+//
 const app = require("./app");
 const port = process.env.PORT || 3000;
 
 app.listen(port);
-console.log("Server running (3000)");
+console.log(`Server running ${port}`);
